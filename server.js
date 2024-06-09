4cef1ff1b6cee7ed6e56d79230ba556ab95a1096
Rylee Jolley <you@example.com>
2024-06-09 12:33:26 -0600

    created new handlebars and updated existing files as well as server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//import the required modules
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -5,22 +6,21 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+// import database connection
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// create and Express application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// configure Handlebars engine with custom helper functions
 const hbs = exphbs.create({ helpers});
 
+// Session configuration
 const sess = {
     secret: 'Super secret secret',
-    cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
+    cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -28,17 +28,25 @@ const sess = {
     })
   };
 
+// use sessions in the app
 app.use(session(sess));
 
+// set up Handlears as the view engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// middleware for parsing JSON and URLencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
 
+
+// syncing the Sequelize models with the database and starting the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening at http://localhost:3001/'));
 });
