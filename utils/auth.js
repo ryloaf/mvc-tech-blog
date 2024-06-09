5f838a3ca8f19d1bf66a93f43e9573a1248478b8
Rylee Jolley <you@example.com>
2024-06-09 15:19:21 -0600

    bug fixes and styling

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 // middleware function to check if the user is authenticated
 const withAuth = (req, res, next) => {
-  if (!req.session.logged_in) {
+  if (!req.session.loggedIn) {
     res.redirect('/login');
   } else {
     next();
